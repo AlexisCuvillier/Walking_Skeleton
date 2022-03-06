e7a8641e4925b21929f41999b68060b2dc57c972
AlexisCuvillier <alexis.cuvillier@protonmail.com>
2022-03-06 11:34:01 +0100

    addFormik

--- client-app/src/features/activities/dashborard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashborard/ActivityListItem.tsx
@@ -1,4 +1,5 @@
 
+import { format } from "date-fns";
 import { Link } from "react-router-dom";
 import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../../App/models/activity";
@@ -29,7 +30,7 @@ export default function ActivityListItem({ activity }: Props) {
       </Segment>
       <Segment>
         <span>
-          <Icon name="clock" /> {activity.date}
+          <Icon name="clock" /> {format(activity.date!, 'dd MMM yyyy h:mm aa')}
           <Icon name="marker" />
           {activity.venue}
         </span>
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,12 +2,14 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-calendar/dist/Calendar.css';
 import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-datepicker/dist/react-datepicker.css'
 import './App/layout/styles.css';
 import App from './App/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './App/stores/store';
 import {  Router } from 'react-router-dom';
 import {createBrowserHistory} from 'history'
+
 
 
 export const history = createBrowserHistory();
